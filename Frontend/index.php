948848fd2788a1533a0185c41bdd3c4bcb224f06
MohamedABBJ <MohamedABBJ>
2023-01-23 19:10:00 -0400

    refactoring

--- Frontend/index.php
+++ Frontend/index.php
@@ -70,7 +70,6 @@
                     </div>
             ");
 
-            //Toca cambiar los botones a form e intentar hacer lo que puse abajo, lo hare mañana buen trabajo.
             }
             
         }
@@ -80,7 +79,25 @@
     <h1>Notes</h1>
     <div class="notes" id="notes">
         </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
         <script src="index.js"></script>
+        <script text="javascript">
+            noteButtons = document.getElementsByClassName('Notebtn')
+    for (let i = 0; i < noteButtons.length; i++) {
+        noteButtons[i].onclick = function (e) {
+        var noteButtonTitleValue = 2
+        $.ajax({
+            url:"./Components/php/clickNote.php",
+            method:"POST",
+            dataType:'text',
+            data:{'noteButtonTitleValue' : noteButtonTitleValue},
+            success: function(data){
+                location.href = "./Components/php/clickNote.php"
+            }
+        })
+    }
+        //Buscar notetitlevalue en la base de datos y poner los valores en clicknote para que aparezcan alla correctamente.
+    }
     </script>
 </body>
 
