a8433e77aa72af02babe5a5045e05e920f86ba60
MohamedABBJ <MohamedABBJ>
2023-02-03 12:46:22 -0400

    Disable inputs if editing note sucess

--- Backend/Components/Notes/editNote.php
+++ Backend/Components/Notes/editNote.php
@@ -10,9 +10,9 @@
     <h1>Editing Note</h1>
     <p>Leave title or description in blank if you don't want to edit it</p>
     <form action='' method="post">
-        New title <input type="text" name="newTitle" value="" id=""><br>
+        New title <input type="text" name="newTitle" value="" id="newTitle"><br>
         <br>
-        New description <input type="text" name="newDescription" value="" id=""><br>
+        New description <input type="text" name="newDescription" id="newDescription" value=""><br>
         <button type="submit">Edit Note</button>
     </form>
     <br>
@@ -22,17 +22,30 @@
     include(__DIR__ . "../../DB/dbConection.php");
     $newTitle = $_POST['newTitle'] ?? NULL;
     $newDescription = $_POST['newDescription'] ?? NULL;
+
     session_start();
     $idUser = $_SESSION['iduser'] ?? NULL;
     $noteId = $_SESSION['noteId'] ?? NULL;
     
+    
+    echo("<script>
+    let disableInputs = () =>{
+    newTitle = document.getElementById('newTitle');
+    newTitle.setAttribute('disabled', '');
+    newDescription = document.getElementById('newDescription');
+    newDescription.setAttribute('disabled', '');}
+    </script>");
+
     //Editing both
     if(!empty($newDescription) && !empty($newTitle)){
         $editNote = $db -> prepare("UPDATE `notes-app`.`notesuser($idUser)` SET `notetitle` = :noteTitle, `notedescription` = :noteDescription  WHERE idnotesuser = $noteId");
         $editNote->bindParam(':noteTitle', $newTitle);
         $editNote->bindParam(':noteDescription', $newDescription);
         $editNote->execute();
-        echo("Note Title and Description has been edited successfully");
+        echo("<script>
+        disableInputs();
+        </script>");
+        echo("Note title and description has been edited successfully <br>");
         echo("Wait a moment, redirecting...");
         header("refresh:2;url='../../index.php");
     }
@@ -41,7 +54,10 @@
             $editNote = $db -> prepare("UPDATE `notes-app`.`notesuser($idUser)` SET `notetitle` = :noteTitle  WHERE idnotesuser = $noteId");
             $editNote->bindParam(':noteTitle', $newTitle);
             $editNote->execute();
-            echo("Note Title has been edited successfully");
+            echo("<script>
+            disableInputs();
+            </script>");
+            echo("Note title has been edited successfully");
             echo("Wait a moment, redirecting...");
             header("refresh:2;url='../../index.php");
         }
@@ -50,7 +66,10 @@
             $editNote = $db -> prepare("UPDATE `notes-app`.`notesuser($idUser)` SET `notedescription` = :noteDescription  WHERE idnotesuser = $noteId");
             $editNote->bindParam(':noteDescription', $newDescription);
             $editNote->execute();
-            echo("Note Description has been edited successfully");
+            echo("<script>
+            disableInputs();
+            </script>");
+            echo("Note description has been edited successfully");
             echo("Wait a moment, redirecting...");
             header("refresh:2;url='../../index.php");
         }
