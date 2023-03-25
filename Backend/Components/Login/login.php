833552740e6d367b6ad6bb310eef5682d773f44f
MohamedABBJ <MohamedABBJ>
2023-03-25 16:37:43 -0400

    Fixing login not showing anything if user and password are incorrect

--- Backend/Components/Login/login.php
+++ Backend/Components/Login/login.php
@@ -25,58 +25,60 @@
 
     echo ("<script>alert('You cannot leave both inputs in blank!!')</script>");
     header("refresh:0;url='../../../Frontend/Components/logIn/login.html");
-  } else if (password_verify($password, $loginPassword)) {
-    $loginQuery = $db->prepare("SELECT username AND password FROM `notes-app`.`users` WHERE username = :username AND password = :password");
-    $loginQuery->bindParam(':username', $username);
-    $loginQuery->bindParam(':password', $loginPassword);
-    $loginQuery->execute();
-
-
-    $idUserQuery = $db->prepare("SELECT idusers FROM `notes-app`.`users` WHERE username = :username AND password = :password");
-    $idUserQuery->bindParam(':username', $username);
-    $idUserQuery->bindParam(':password', $loginPassword);
-    $idUserQuery->execute();
-    $idUser = $idUserQuery->fetchColumn();
-    session_start();
-    $_SESSION['iduser'] = $idUser;
-
-
-    if ($loginQuery->rowCount() > 0 && $username !== NULL && $password !== NULL) {
-      $loggedInQuery = $db->prepare("UPDATE `notes-app`.`users` SET loggedstatus = 1 WHERE username = :username AND password = :password");
-      $loggedInQuery->bindParam(':username', $username);
-      $loggedInQuery->bindParam(':password', $loginPassword);
-      $loggedInQuery->execute();
-
-      $loginStatusQuery = $db->prepare("SELECT loggedstatus FROM `notes-app`.`users` WHERE username = :username AND password = :password");
-      $loginStatusQuery->bindParam(':username', $username);
-      $loginStatusQuery->bindParam(':password', $loginPassword);
-      $loginStatusQuery->execute();
-      $loginStatus = $loginStatusQuery->fetchColumn();
-
-      $loginNameQuery = $db->prepare("SELECT name FROM `notes-app`.`users` WHERE username = :username AND password = :password");
-      $loginNameQuery->bindParam(':username', $username);
-      $loginNameQuery->bindParam(':password', $loginPassword);
-      $loginNameQuery->execute();
-      $loginName = $loginNameQuery->fetchColumn();
-
-      $maxUserNotesIdQuery = $db->prepare("SELECT MAX(idnotesuser) AS maxUserNotesId FROM `notes-app`.`notesuser($idUser)`");
-      $maxUserNotesIdQuery->execute();
-      $maxUserNotesIdQuery = $maxUserNotesIdQuery->fetch(PDO::FETCH_ASSOC);
-      $maxUserNotesId = $maxUserNotesIdQuery['maxUserNotesId'];
-
-      $_SESSION['maxUserNotesId'] = $maxUserNotesId;
-      $_SESSION['username'] = $username;
-      $_SESSION['password'] = $loginPassword;
-      $_SESSION['loginStatus'] = $loginStatus;
-      $_SESSION['loginName'] = $loginName;
-
-      echo 'You have been loged to the system welcome!';
-      header("refresh:2;url='../../index.php");
-    } else if (isset($username)) {
-      echo 'Username or password are incorrect!';
-      echo var_dump($password);
-    }
+  }else{
+    if (password_verify($password, $loginPassword)) {
+      $loginQuery = $db->prepare("SELECT username AND password FROM `notes-app`.`users` WHERE username = :username AND password = :password");
+      $loginQuery->bindParam(':username', $username);
+      $loginQuery->bindParam(':password', $loginPassword);
+      $loginQuery->execute();
+  
+  
+      $idUserQuery = $db->prepare("SELECT idusers FROM `notes-app`.`users` WHERE username = :username AND password = :password");
+      $idUserQuery->bindParam(':username', $username);
+      $idUserQuery->bindParam(':password', $loginPassword);
+      $idUserQuery->execute();
+      $idUser = $idUserQuery->fetchColumn();
+      session_start();
+      $_SESSION['iduser'] = $idUser;
+  
+      if ($loginQuery->rowCount() > 0 && $username !== NULL && $password !== NULL) {
+        $loggedInQuery = $db->prepare("UPDATE `notes-app`.`users` SET loggedstatus = 1 WHERE username = :username AND password = :password");
+        $loggedInQuery->bindParam(':username', $username);
+        $loggedInQuery->bindParam(':password', $loginPassword);
+        $loggedInQuery->execute();
+  
+        $loginStatusQuery = $db->prepare("SELECT loggedstatus FROM `notes-app`.`users` WHERE username = :username AND password = :password");
+        $loginStatusQuery->bindParam(':username', $username);
+        $loginStatusQuery->bindParam(':password', $loginPassword);
+        $loginStatusQuery->execute();
+        $loginStatus = $loginStatusQuery->fetchColumn();
+  
+        $loginNameQuery = $db->prepare("SELECT name FROM `notes-app`.`users` WHERE username = :username AND password = :password");
+        $loginNameQuery->bindParam(':username', $username);
+        $loginNameQuery->bindParam(':password', $loginPassword);
+        $loginNameQuery->execute();
+        $loginName = $loginNameQuery->fetchColumn();
+  
+        $maxUserNotesIdQuery = $db->prepare("SELECT MAX(idnotesuser) AS maxUserNotesId FROM `notes-app`.`notesuser($idUser)`");
+        $maxUserNotesIdQuery->execute();
+        $maxUserNotesIdQuery = $maxUserNotesIdQuery->fetch(PDO::FETCH_ASSOC);
+        $maxUserNotesId = $maxUserNotesIdQuery['maxUserNotesId'];
+  
+        $_SESSION['maxUserNotesId'] = $maxUserNotesId;
+        $_SESSION['username'] = $username;
+        $_SESSION['password'] = $loginPassword;
+        $_SESSION['loginStatus'] = $loginStatus;
+        $_SESSION['loginName'] = $loginName;
+  
+        echo 'You have been loged to the system welcome!';
+        header("refresh:2;url='../../index.php");
+  
+      }
+  }else {
+    echo 'Username or password are incorrect!';
+    echo var_dump($password);
   }
+}
   ?>
 </body>
 
